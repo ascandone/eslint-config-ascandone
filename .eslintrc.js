abd602b3e4d701ca5ef8618ae673bc8ab9641d93
ascandone <alescandone@gmail.com>
2022-05-18 15:31:28 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	extends: [
+		"airbnb",
+		"eslint:recommended",
+		"plugin:import/errors",
+		// "plugin:react/recommended",
+		"plugin:jsx-a11y/recommended",
+		"prettier",
+		// "plugin:prettier/recommended",
+	],
+	// plugins: ["react-hooks"],
+	rules: {},
+};
---
+++ readme.md
@@ -0,0 +1,22 @@
+# How to use
+
+**Step 1**:
+
+Add the following to your `.eslintrc.js`:
+
+```diff
+module.exports = {
++ extends: ["ascandone"],
+};
+
+```
+
+**Step 2**:
+
+```bash
+echo {}> .prettierrc.json
+```
+
+**Step 3**:
+
+Profit 🚀
